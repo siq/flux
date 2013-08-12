aa16312de21f7653d7e8674bc4d4a16a54cf7eba
shola esho <sesho@us.ibm.com>
2013-08-12 07:11:02 -0500

    DIQ-217 fixed foreign key cascade issues when reseting old concert workflows

--- flux/migrations/versions/2a7c3bf7b40_remove_old_concert_w.py
+++ flux/migrations/versions/2a7c3bf7b40_remove_old_concert_w.py
@@ -13,6 +13,30 @@ from spire.schema.fields import *
 from sqlalchemy.dialects import postgresql
 
 def upgrade():
+    op.execute("delete from execution where run_id in "
+               "(select id from run where workflow_id in "
+               "(select id from workflow where (id, name) in ("
+               "('00000000-0000-0000-0000-000000002000','create-identification-box'),"
+               "('00000000-0000-0000-0000-000000002001','create-artifact'),"
+               "('00000000-0000-0000-0000-000000002002','create-collection-box'),"
+               "('00000000-0000-0000-0000-000000002003','create-export-box'),"
+               "('00000000-0000-0000-0000-000000002004','refine-box'),"
+               "('00000000-0000-0000-0000-000000002005','restart-box'),"
+               "('00000000-0000-0000-0000-000000002006','clone-box'),"
+               "('00000000-0000-0000-0000-000000002007','delete-box'))))")
+
+    op.execute("delete from product where run_id in "
+               "(select id from run where workflow_id in "
+               "(select id from workflow where (id, name) in ("
+               "('00000000-0000-0000-0000-000000002000','create-identification-box'),"
+               "('00000000-0000-0000-0000-000000002001','create-artifact'),"
+               "('00000000-0000-0000-0000-000000002002','create-collection-box'),"
+               "('00000000-0000-0000-0000-000000002003','create-export-box'),"
+               "('00000000-0000-0000-0000-000000002004','refine-box'),"
+               "('00000000-0000-0000-0000-000000002005','restart-box'),"
+               "('00000000-0000-0000-0000-000000002006','clone-box'),"
+               "('00000000-0000-0000-0000-000000002007','delete-box'))))")
+
     op.execute("delete from run where workflow_id in "
                "(select id from workflow where (id, name) in ("
                "('00000000-0000-0000-0000-000000002000','create-identification-box'),"
